74d6c0f86044b86487cff50906f83c545c7c02d8
komi-w10 <jmmfcbb@outlook.com>
2024-09-27 00:58:24 -0300

    bridge added

--- src/client/java/kn/jktech/exkinilo/world/noodle.java
+++ src/client/java/kn/jktech/exkinilo/world/noodle.java
@@ -32,68 +32,64 @@ public class noodle implements IChunkProvider {
         float tr=-.3f;
             for (byte xx = 0; xx < 16; xx++) {
                 for (byte yy = 0; yy < 16; yy++) {
-                    for (byte zz = 0; zz < 16; zz++) {
+                    for (byte zz = 0; zz < 16; zz++) {if (blocks[xx << 8 | zz << 4 | yy] ==0)
+                        {
 
 
-                        if (ybridge && xbridge &&xx<6&& yy>=8&&yy<13){
-                            if( yy==8)
-                                blocks[xx << 8 | zz << 4 | yy]=(short)(xx==0||xx==5?146:217);
-                            else blocks[xx << 8 | zz << 4 | yy]=(short)(0);
-                        }else{
-                            double v=noice.noise(((double) (((16 * x)) + xx)) , ((double)((y * 16) + yy)), ((double) ((16 * z) + zz)) );
+                            if (ybridge && xbridge && xx < 6 && yy >= 8 && yy < 13) {
+                                if (yy == 8)
+                                    blocks[xx << 8 | zz << 4 | yy] = (short) (xx == 0 || xx == 5 ? 217 : 146);
+                                else blocks[xx << 8 | zz << 4 | yy] = (short) (0);
+                            } else {
+                                double v = noice.noise(((double) (((16 * x)) + xx)), ((double) ((y * 16) + yy)), ((double) ((16 * z) + zz)));
 
-                            if(v <= tr) {}
-                        double val[] ={
-                                v,
-                                noice.noise(((double) (((16 * x)) + xx)) , ((double)((y * 16) + yy+1)), ((double) ((16 * z) + zz)) ),
-                                noice.noise(((double) (((16 * x)) + xx)) , ((double)((y * 16) + yy+2)), ((double) ((16 * z) + zz)) ),
-                                noice.noise(((double) (((16 * x)) + xx)) , ((double)((y * 16) + yy-1)), ((double) ((16 * z) + zz)) ),
-                                noice.noise(((double) (((16 * x)) + xx)) , ((double)((y * 16) + yy-2)), ((double) ((16 * z) + zz)) ),
-                        };/*
-
-                        float val2=1;
-                        val2+=val[3] <= tr?1:0;
-                        val2+=val[4] <= tr?1:0;
-                        val[0]+=val2/10f;
-                        val[1]+=val2/10f;
-                        val[2]+=val2/10f;
+                                if (v <= tr) {
+                                    double val[] = {
+                                            v,
+                                            noice.noise(((double) (((16 * x)) + xx)), ((double) ((y * 16) + yy + 1)), ((double) ((16 * z) + zz))),
+                                            noice.noise(((double) (((16 * x)) + xx)), ((double) ((y * 16) + yy + 2)), ((double) ((16 * z) + zz))),
+                                    };
 
 
-                            if (val[1] > tr&&val[2] > tr) {
-                                blocks[xx << 8 | zz << 4 | yy] = (short)(blockup);
-                            }
-                            else if (val[1] <= tr&&val[2] > tr) {
-                                blocks[xx << 8 | zz << 4 | yy] = (short)(blockmid);
-                            }else {
-                                blocks[xx << 8 | zz << 4 | yy] = (short)(block);}
+                                    if (val[1] > tr && val[2] > tr) {
 
-                        }else {*/
-                            if (zbridge&&xbridge&&y<=4&&xx<6){
-
-                                if (ybridge&&yy<8){
-                                    switch (yy){
-                                        case 7:
-                                            blocks[xx << 8 | zz << 4 | yy]=(short)(146);
-                                            break;
-                                        case 6:
-                                            blocks[xx << 8 | zz << 4 | yy]=(short)(xx>0&&xx<5&&zz<5
-                                                    ?0:146);
-                                            break;
-                                        default:
-                                            blocks[xx << 8 | zz << 4 | yy]=(short)(xx>=2&&xx<=3&&zz<=3&&zz>=2
-                                                    ?146:0);
-                                            break;
-
+                                    } else
+                                    if (val[1] <= tr && val[2] > tr) {
+                                        blocks[xx << 8 | zz << 4 | yy] = (short) (blockup);
+                                        blocks[xx << 8 | zz << 4 | (yy>0?yy-1:yy)] = (short) (blockmid);
+                                        blocks[xx << 8 | zz << 4 | (yy>1?yy-2:yy)] = (short) (blockmid);
+                                    } else {
+                                        blocks[xx << 8 | zz << 4 | yy] = (short) (block);
                                     }
-                                }else if (!ybridge){
-                                    blocks[xx << 8 | zz << 4 | yy]=(short)(xx>=2&&xx<=3&&zz<=3&&zz>=2
-                                            ?146:0);
 
+                                } else {
+                                    if (zbridge && xbridge && y <= 4 && xx < 6) {
+
+                                        if (ybridge && yy < 8) {
+                                            switch (yy) {
+                                                case 7:
+                                                    blocks[xx << 8 | zz << 4 | yy] = (short) (zz > 5 ? 0 : 146);
+                                                    break;
+                                                case 6:
+                                                    blocks[xx << 8 | zz << 4 | yy] = (short) (xx == 0 || xx == 5 || zz > 4 || zz < 1
+                                                            ? 0 : 146);
+                                                    break;
+                                                default:
+                                                    blocks[xx << 8 | zz << 4 | yy] = (short) (xx < 2 || xx > 3 || zz > 3 || zz < 2
+                                                            ? 0 : 146);
+                                                    break;
+
+                                            }
+                                        } else if (!ybridge) {
+                                            blocks[xx << 8 | zz << 4 | yy] = (short) (xx < 2 || xx > 3 || zz > 3 || zz < 2
+                                                    ? 0 : 146);
+
+                                        }
+                                    } else blocks[xx << 8 | zz << 4 | yy] = (short) (0);
                                 }
                             }
-                            else blocks[xx << 8 | zz << 4 | yy] = (short)(0);}
 
-
+                        }
                 }
             }
         }
@@ -131,8 +127,8 @@ public class noodle implements IChunkProvider {
     @Override
     public Chunk provideChunk(int x, int y, int z) {
         short[] blocks = new short[4096];
-        this.generateTerrain(x, y, z, blocks);
         Chunk chunk = new Chunk(this.worldObj, blocks, x, y, z);
+        this.generateTerrain(x, y, z, blocks);
         chunk.prepareLighting();
         return chunk;
     }
